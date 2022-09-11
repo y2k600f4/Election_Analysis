b1eb4d9af7377b35beb5d72dc7215b5b266aab97
Ken Gaynor <Ken.Gaynor@qorvo.com>
2022-09-11 17:54:06 -0400

    adding the election analysis and cleanup.

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-#Final Python Script
-# The data we need to retrieve.
-# 1. The total number of votes cast
-# 2. A complete list of candidates who received votes
-# 3. The percentge of votes each candidate won
-# 4. The total number of votes each candidate won
-# 5. The winner of the election based on popular vote.
-
 # Add our dependencies.
 import csv
 import os
